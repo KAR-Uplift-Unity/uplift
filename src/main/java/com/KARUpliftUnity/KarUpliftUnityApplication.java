2e22df816079e07a730b3137dd707ba56cfec1d8
Aaron Bond <aaron.reid.bond@gmail.com>
2023-08-17 08:29:00 -0700

    start

---
+++ src/main/java/com/KARUpliftUnity/KarUpliftUnityApplication.java
@@ -0,0 +1,13 @@
+package com.KARUpliftUnity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarUpliftUnityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarUpliftUnityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KARUpliftUnity/KarUpliftUnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KARUpliftUnity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarUpliftUnityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
