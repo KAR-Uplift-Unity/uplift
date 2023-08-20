4cb4ee212ed6ef77d1545c032c182fe24423b7ce
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-08-20 15:22:34 -0500

    Altered contact_us controller to accept form submission

--- src/main/java/com/KARUpliftUnity/controllers/HomeController.java
+++ src/main/java/com/KARUpliftUnity/controllers/HomeController.java
@@ -1,12 +1,20 @@
 package com.KARUpliftUnity.controllers;
 
+import com.KARUpliftUnity.models.ContactForm;
+import com.KARUpliftUnity.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmailService emailService;
 
     @GetMapping("/")
     public String returnHomePage(){
@@ -15,11 +23,18 @@ public class HomeController {
 
     @GetMapping("/mission")
     public String showMissionPage() {
-        return "mission"; // Assuming you have a Thymeleaf template named "mission.html"
+        return "mission";
     }
 
     @GetMapping("/contact_us")
-    public String showContactUsPage() {
-        return "contact_us"; // Assuming you have a Thymeleaf template named "contact_us.html"
+    public String showContactUsPage(Model model) {
+        model.addAttribute("contactForm", new ContactForm());
+        return "contact_us";
+    }
+
+    @PostMapping("/contact_us")
+    public String handleContactUsForm(@ModelAttribute ContactForm form) {
+        emailService.sendContactFormEmail(form);
+        return "/contact_us";
     }
 }
