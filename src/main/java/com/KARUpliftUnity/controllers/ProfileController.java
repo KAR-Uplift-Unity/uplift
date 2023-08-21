8c9ded5d206e1c6e719db464c77b12bf4f1d979c
Aaron Bond <aaron.reid.bond@gmail.com>
2023-08-21 14:57:38 -0700

    profile controller and admin check on all pages

---
+++ src/main/java/com/KARUpliftUnity/controllers/ProfileController.java
@@ -0,0 +1,45 @@
+package com.KARUpliftUnity.controllers;
+
+import com.KARUpliftUnity.models.User;
+import com.KARUpliftUnity.repositories.PostRepository;
+import com.KARUpliftUnity.repositories.TagRepository;
+import com.KARUpliftUnity.repositories.UserRepository;
+import com.KARUpliftUnity.services.EmailService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    private final PostRepository postDao;
+
+    private final UserRepository userDao;
+
+    public ProfileController(PostRepository postDao, UserRepository userDao) {
+        this.postDao = postDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/profile")
+    public String showProfile(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        boolean admin  = user.getAdmin();
+        if (admin) {
+            model.addAttribute("admin", 1);
+        }
+        model.addAttribute("posts", postDao.getAllByUserId(user.getId()));
+        return "/users/profile";
+    }
+
+    @GetMapping("/profile/settings")
+    public String showProfileSettings(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        boolean admin  = user.getAdmin();
+        if (admin) {
+            model.addAttribute("admin", 1);
+        }
+        return "/users/profile_settings";
+    }
+}
