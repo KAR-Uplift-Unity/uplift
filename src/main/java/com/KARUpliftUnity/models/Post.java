3fa4984df09e307f64e03c67186e1cd4efc1f5a6
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-08-22 09:38:12 -0500

    Working on saving edited tags to database

--- src/main/java/com/KARUpliftUnity/models/Post.java
+++ src/main/java/com/KARUpliftUnity/models/Post.java
@@ -38,6 +38,9 @@ public class Post {
     @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "post")
     private List<Tag> tags;
 
+    @Transient
+    private String tagString;
+
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name="post_categories",
@@ -85,6 +88,14 @@ public class Post {
 
     public void setCategories(List<Category> categories) {
         this.categories = categories;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
     }
 
     public long getId() {
@@ -153,6 +164,14 @@ public class Post {
 
     public long getIdByTitle(String title){
         return id;
+    }
+
+    public String getTagString() {
+        return tagString;
+    }
+
+    public void setTagString(String tagString) {
+        this.tagString = tagString;
     }
 }
 
