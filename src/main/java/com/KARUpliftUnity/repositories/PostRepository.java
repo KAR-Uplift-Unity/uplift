2f45a2ec5567f6ce2313f1d35b9b166bd33a569d
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-08-23 13:40:07 -0500

    Added new methods to repository

--- src/main/java/com/KARUpliftUnity/repositories/PostRepository.java
+++ src/main/java/com/KARUpliftUnity/repositories/PostRepository.java
@@ -3,10 +3,13 @@ package com.KARUpliftUnity.repositories;
 import com.KARUpliftUnity.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long>{
     Post getIdByTitle(String title);
     Post getAllByUserId (long id);
-
     Post findPostById(Long id);
+    List<Post> findByUserIdAndArchiveFalse(Long userId);
+    List<Post> findByUserIdAndArchiveTrue(Long userId);
 
 }
