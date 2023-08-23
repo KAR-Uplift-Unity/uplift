795aff5266c2999861b46faf6cf07d7ac36c6196
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-08-23 13:41:11 -0500

    Created the archive and unarchive controller

--- src/main/java/com/KARUpliftUnity/controllers/ProfileController.java
+++ src/main/java/com/KARUpliftUnity/controllers/ProfileController.java
@@ -1,5 +1,6 @@
 package com.KARUpliftUnity.controllers;
 
+import com.KARUpliftUnity.models.Post;
 import com.KARUpliftUnity.models.User;
 import com.KARUpliftUnity.repositories.PostRepository;
 import com.KARUpliftUnity.repositories.TagRepository;
@@ -9,6 +10,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.regex.Pattern;
 
 @Controller
 public class ProfileController {
@@ -29,8 +36,30 @@ public class ProfileController {
         if (admin) {
             model.addAttribute("admin", 1);
         }
-        model.addAttribute("posts", postDao.getAllByUserId(user.getId()));
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("activePosts", postDao.findByUserIdAndArchiveFalse(user.getId()));
+        model.addAttribute("archivedPosts", postDao.findByUserIdAndArchiveTrue(user.getId()));
         return "/users/profile";
+    }
+
+    @PostMapping("/posts/{id}/archive")
+    public String archivePost(@PathVariable long id) {
+        Post post = postDao.findById(id).orElse(null);
+        if (post != null) {
+            post.setArchive(true);
+            postDao.save(post);
+        }
+        return "redirect:/profile";
+    }
+
+    @PostMapping("/posts/{id}/unarchive")
+    public String unarchivePost(@PathVariable long id) {
+        Post post = postDao.findById(id).orElse(null);
+        if (post != null) {
+            post.setArchive(false);
+            postDao.save(post);
+        }
+        return "redirect:/profile";
     }
 
     @GetMapping("/settings")
@@ -40,6 +69,8 @@ public class ProfileController {
         if (admin) {
             model.addAttribute("admin", 1);
         }
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
         return "/users/profile_settings";
     }
 }
