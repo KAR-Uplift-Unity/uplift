49920be7879d99069d79ad3df2336d38eec5f777
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-08-23 16:06:44 -0500

    Added HttpServletRequest to login

--- src/main/java/com/KARUpliftUnity/controllers/AuthenticationController.java
+++ src/main/java/com/KARUpliftUnity/controllers/AuthenticationController.java
@@ -1,12 +1,18 @@
 package com.KARUpliftUnity.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
-    public String showLoginForm() {
+    public String showLoginForm(HttpServletRequest request) {
+        String previousUrl = (String) request.getSession().getAttribute("previousUrl");
+        if (previousUrl != null) {
+            request.getSession().removeAttribute("previousUrl");
+            return "redirect:" + previousUrl;
+        }
         return "login/login";
     }
 }
