4eb71758c6711e070385cb97772cfabe244cb8b0
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-08-23 16:08:39 -0500

    Testing intended redirects

---
+++ src/main/java/com/KARUpliftUnity/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.KARUpliftUnity.config;
+
+import com.KARUpliftUnity.interceptors.AuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthenticationInterceptor authenticationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .excludePathPatterns("/", "/posts", "/posts/*", "/signup", "/login", "/contact_us", "/mission","/css/**", "/js/**", "/images/**");
+    }
+}
+
