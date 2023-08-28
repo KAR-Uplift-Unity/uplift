2f56a8a93acf0ca0960019c2072549df82447c1d
Aaron Bond <aaron.reid.bond@gmail.com>
2023-08-28 13:31:46 -0700

    added comment and post del and change flag

--- src/main/java/com/KARUpliftUnity/controllers/AdminController.java
+++ src/main/java/com/KARUpliftUnity/controllers/AdminController.java
@@ -48,9 +48,13 @@ public class AdminController {
         boolean admin  = user.getAdmin();
         if (admin) {
             List<User> allUsers = userDao.findAll();
-            List<Post> flagged = postDao.findAllByFlaggedIsTrue();
+            List<Post> flaggedP = postDao.findAllByFlaggedIsTrue();
+            System.out.println("flaggedP = " + flaggedP);
+            List<Comment> flaggedC = comDao.findAllByFlaggedIsTrue();
+            System.out.println("flaggedC = " + flaggedC);
             model.addAttribute("users", allUsers);
-            model.addAttribute("flagged",flagged);
+            model.addAttribute("flaggedP", flaggedP);
+            model.addAttribute("flaggedC", flaggedC);
             return "admin/index";
         }
         return "redirect:/";
@@ -116,7 +120,53 @@ public class AdminController {
                 catDao.deleteAllInBatch(categories);
             }
 
+            List<Like> likes = likeDao.findAllByPost(post);
+            if (likes != null){
+                likeDao.deleteAllInBatch(likes);
+            }
+
+            List<Comment> comments = comDao.findAllByPost(post);
+            if (comments != null){
+                comDao.deleteAllInBatch(comments);
+            }
+
             postDao.delete(post);
+        }
+        return "redirect:/admin-panel";
+    }
+
+    @PostMapping ("/admin-panel/posts/{id}/un-flag")
+    public String postUnFlag(@PathVariable long id){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        boolean admin  = user.getAdmin();
+        if (admin) {
+            Post post = postDao.findPostById(id);
+            post.setFlagged(false);
+            post.setReportReason(null);
+            postDao.save(post);
+        }
+        return "redirect:/admin-panel";
+    }
+
+    @PostMapping ("/admin-panel/comments/{id}")
+    public String dropComment(@PathVariable long id){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        boolean admin  = user.getAdmin();
+        if (admin) {
+            Comment comment = comDao.findById(id);
+            comDao.delete(comment);
+        }
+        return "redirect:/admin-panel";
+    }
+    @PostMapping ("/admin-panel/comments/{id}/un-flag")
+    public String commentUnFlag(@PathVariable long id){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        boolean admin  = user.getAdmin();
+        if (admin) {
+            Comment comment = comDao.findById(id);
+            comment.setFlagged(false);
+            comment.setReportReason(null);
+            comDao.save(comment);
         }
         return "redirect:/admin-panel";
     }
