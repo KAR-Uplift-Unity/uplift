c5f63aa9ace239e0708d42679e432f483be4bed4
Aaron Bond <aaron.reid.bond@gmail.com>
2023-08-30 14:43:31 -0700

    sql seeder notes

--- seeder.sql
+++ seeder.sql
@@ -1,3 +1,12 @@
+# before you use the seeder file
+# step 1: use the uplift-db-migration file (if you already have the db fallow step 2 then drop the DB as well.)
+# step 2: (drop all tables in db in order 1.post_categories, 2.categories, 3.comments, 4.hearts, 5.images, 6.tags, 7.updates, 8.posts, 9.users)
+# step 2 continued: if you dont have tables in the db run the application (NOT THE SQL SEEDER) so that the tables are generated.
+# step 3: run this seeder file!
+# step 4; Please make sure that the "flagged" column on both the comments, and post are set to false. all should be good to go!
+
+
+
 USE uplift_db;
 
 INSERT INTO categories (id ,cat) VALUES
